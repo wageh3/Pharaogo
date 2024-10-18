552e239968cef9e673fb89d792de28d57b4902cc
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2024-10-18 17:59:39 +0300

    Add whishlist

---
+++ WebApplication7/Models/WishList.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication7.Models
+{
+    public class WishList
+    {
+        [ForeignKey("place")]
+        public int PlaceId { get; set; }
+        public Place place { get; set; }
+        [ForeignKey("user")]
+        public int UserID { get; set; }
+        public User user { get; set; }
+    }
+}
