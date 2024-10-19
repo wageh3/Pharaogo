1ae79e5446769ad109cc27befe48e92973393033
Esraa156 <eo6344848@gmail.com>
2024-10-19 22:46:25 +0300

    Role Data Seed

--- WebApplication7/Controllers/AccountController.cs
+++ WebApplication7/Controllers/AccountController.cs
@@ -1,63 +1,68 @@
-﻿using WebApplication7.Controllers;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using WebApplication7.Models;
 using WebApplication7.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
 
 namespace WebApplication7.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly SignInManager<ApplicationUser> signInManager;
-		private readonly IWebHostEnvironment _webHostEnvironment;
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            IWebHostEnvironment webHostEnvironment, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _webHostEnvironment = webHostEnvironment;
+            _roleManager = roleManager;
+        }
 
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
 
-		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> _signInManager, IWebHostEnvironment webHostEnvironment)
-		{
-			_userManager = userManager;
-			signInManager = _signInManager;
-			_webHostEnvironment = webHostEnvironment;
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel Userrvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUser = await _userManager.FindByEmailAsync(Userrvm.Email);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("Email", "Email is already taken.");
+                    return View(Userrvm);
+                }
 
+                ApplicationUser userModel = new ApplicationUser
+                {
+                    UserName = Userrvm.UserName,
+                    Email = Userrvm.Email
+                };
 
-		}
-		[HttpGet]
-		public IActionResult Register()
-		{
-			return View();
-		}
+                IdentityResult result = await _userManager.CreateAsync(userModel, Userrvm.Password);
 
-		[HttpPost]
-		public async Task<IActionResult> Register(RegisterViewModel Userrvm)
-		{
+                if (result.Succeeded)
+                {
+                    // Ensure roles exist before assigning
+                    if (!await _roleManager.RoleExistsAsync("Admin"))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                    }
 
-			if (ModelState.IsValid)
-			{
-				// Check if the email already exists
-				var existingUser = await _userManager.FindByEmailAsync(Userrvm.Email);
-				if (existingUser != null)
-				{
-					ModelState.AddModelError("Email", "Email is already taken.");
-					return View(Userrvm);
-				}
-				User userModel = new User();
-				userModel.UserName = Userrvm.UserName;
-				userModel.PasswordHash = Userrvm.Password;
-				userModel.Email = Userrvm.Email;
-				//if (Userrvm.Image != null)
-				//{
-				//	var unfile = ImageSaver.SaveImage(Userrvm.Image, _webHostEnvironment);
-				//	userModel.ImageUrl = unfile.Result;
+                    if (!await _roleManager.RoleExistsAsync("Visitor"))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("Visitor"));
+                    }
 
-
-				//}
-				IdentityResult result = await _userManager.CreateAsync(userModel, Userrvm.Password);
-
-				if (result.Succeeded == true)
-				{
                     if (Userrvm.Email == "admiin@gmail.com")
                     {
                         await _userManager.AddToRoleAsync(userModel, "Admin");
@@ -67,75 +72,58 @@ namespace WebApplication7.Controllers
                         await _userManager.AddToRoleAsync(userModel, "Visitor");
                     }
 
+                    await _signInManager.SignInAsync(userModel, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(Userrvm);
+        }
 
-                    //Create Cookie
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-                    await signInManager.SignInAsync(userModel, isPersistent: false);
-
-
-					return RedirectToAction("Index", "Home");
-
-				}
-				else
-				{
-					foreach (var item in result.Errors)
-					{
-						ModelState.AddModelError("", item.Description);
-					}
-				}
-			}
-			return View(Userrvm);
-		}
-		[HttpGet]
-
-		public IActionResult Login()
-		{
-
-			return View();
-		}
-		[HttpPost]
+        [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginViewModel)
         {
             if (ModelState.IsValid)
             {
-                // Find user by email
                 ApplicationUser user = await _userManager.FindByEmailAsync(loginViewModel.Email);
 
                 if (user != null)
                 {
-                    // Check if the password is correct
                     bool found = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
                     if (found)
                     {
-                        // Sign in the user using SignInManager
-                        await signInManager.SignInAsync(user, loginViewModel.RememberMe);
-
-                        // Redirect to the home page after login
+                        await _signInManager.SignInAsync(user, loginViewModel.RememberMe);
                         return RedirectToAction("Index", "Home");
                     }
                     else
                     {
-                        // Password is incorrect
                         ModelState.AddModelError("Password", "Your password is incorrect.");
                     }
                 }
                 else
                 {
-                    // User does not exist
-                    ModelState.AddModelError("", "User does not exist.");
+                    ModelState.AddModelError("Email", "User does not exist.");
                 }
             }
-
-            // If ModelState is not valid, return the view with errors
             return View(loginViewModel);
         }
 
         public async Task<IActionResult> Logout()
-		{
-			await signInManager.SignOutAsync();
-			return RedirectToAction("Index", "Home");
-		}
-
-
-	}
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
 }
