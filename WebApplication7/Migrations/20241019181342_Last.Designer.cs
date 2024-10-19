102e983bf8c20f47e064e115a28ac142e13868a2
Mohamed Ramadan <mr5890347@gmail.com>
2024-10-19 21:14:19 +0300

    Last migration

--- WebApplication7/Migrations/20241019181342_Last.Designer.cs
+++ WebApplication7/Migrations/20241019181342_Last.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication7.Models;
 
@@ -11,9 +12,11 @@ using WebApplication7.Models;
 namespace WebApplication7.Migrations
 {
     [DbContext(typeof(DepiContext))]
-    partial class DepiContextModelSnapshot : ModelSnapshot
+    [Migration("20241019181342_Last")]
+    partial class Last
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebApplication7/Migrations/20241019181342_Last.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication7.Migrations
+{
+    /// <inheritdoc />
+    public partial class Last : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contact_us");
+
+            migrationBuilder.DropTable(
+                name: "Memberships");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contact_us",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    User_ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contact_us", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_contact_us_AspNetUsers_User_ID",
+                        column: x => x.User_ID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Memberships",
+                columns: table => new
+                {
+                    Membership_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    promotion_ID = table.Column<int>(type: "int", nullable: false),
+                    User_ID = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memberships", x => x.Membership_Id);
+                    table.ForeignKey(
+                        name: "FK_Memberships_AspNetUsers_User_ID",
+                        column: x => x.User_ID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Memberships_Promotions_promotion_ID",
+                        column: x => x.promotion_ID,
+                        principalTable: "Promotions",
+                        principalColumn: "promotion_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contact_us_User_ID",
+                table: "contact_us",
+                column: "User_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_promotion_ID",
+                table: "Memberships",
+                column: "promotion_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Memberships_User_ID",
+                table: "Memberships",
+                column: "User_ID");
+        }
+    }
+}
